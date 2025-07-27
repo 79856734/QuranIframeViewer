f2fca97844bf08aaace7820fa3ee2227ebef1cd8
Mostafa_XS1 <73547259+79856734@users.noreply.github.com>
2025-07-27 18:22:58 +0300

    Optimized for desktop and mobile seperately

--- script.js
+++ script.js
@@ -11,4 +11,5 @@ document.getElementById(`back`).onclick = function(){
 document.getElementById(`goto`).onclick = function(){
     pagenum = document.getElementById(`pagenumber`).value;
     document.getElementById(`myiframe`).setAttribute("src", `https://www.easyquran-eg.com/qrhafs/page/${pagenum}`)
+    document.getElementById(`pagenumber`).value = "";
 }
