1fd13ee5a64a00a74a175b5ff195f4fcd83523f0
Mostafa_XS1 <73547259+79856734@users.noreply.github.com>
2025-07-27 15:51:09 +0300

    Add files via upload

---
+++ script.js
@@ -0,0 +1,14 @@
+let pagenum = 1;
+
+document.getElementById(`next`).onclick = function(){
+    pagenum = pagenum + 1
+    document.getElementById(`myiframe`).setAttribute("src", `https://www.easyquran-eg.com/qrhafs/page/${pagenum}`)
+}
+document.getElementById(`back`).onclick = function(){
+    pagenum = pagenum - 1
+    document.getElementById(`myiframe`).setAttribute("src", `https://www.easyquran-eg.com/qrhafs/page/${pagenum}`)
+}
+document.getElementById(`goto`).onclick = function(){
+    pagenum = document.getElementById(`pagenumber`).value;
+    document.getElementById(`myiframe`).setAttribute("src", `https://www.easyquran-eg.com/qrhafs/page/${pagenum}`)
+}
