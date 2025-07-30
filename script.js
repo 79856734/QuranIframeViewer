5ab72f5682af2dd14603f848795744785afa4059
Mostafa_XS1 <73547259+79856734@users.noreply.github.com>
2025-07-30 20:05:46 +0300

    Add files via upload

--- script.js
+++ script.js
@@ -2,42 +2,65 @@ let pagenum = 1;
 let playerOpen = 0;
 let darkmode = 0;
 
-document.getElementById(`next`).onclick = function(){
-    pagenum = pagenum + 1
-    pagenum >= 605 ? pagenum = 604 : pagenum = pagenum;
-    document.getElementById(`myiframe`).setAttribute("src", `https://www.easyquran-eg.com/qrhafs/page/${pagenum}`)
-}
-document.getElementById(`back`).onclick = function(){
-    pagenum = pagenum - 1
-    pagenum <= 0 ? pagenum = 1 : pagenum = pagenum;
-    document.getElementById(`myiframe`).setAttribute("src", `https://www.easyquran-eg.com/qrhafs/page/${pagenum}`)
-}
-document.getElementById(`goto`).onclick = function(){
-    pagenum = parseInt(document.getElementById(`pagenumber`).value, 10);
-    pagenum <= 0 ? pagenum = 1 : pagenum = pagenum;
-    pagenum >= 605 ? pagenum = 604 : pagenum = pagenum;
-    document.getElementById(`myiframe`).setAttribute("src", `https://www.easyquran-eg.com/qrhafs/page/${pagenum}`)
-    document.getElementById(`pagenumber`).value = "";
-}
+const surahButtons = document.querySelectorAll('.surah-btn');
+if (surahButtons.length > 0) {
+  surahButtons.forEach(btn => {
+    btn.addEventListener('click', () => {
+      const page = parseInt(btn.getAttribute('data-page'), 10);
+      sessionStorage.setItem('quranPage', page);
+      window.location.href = 'index.html';
+    });
+  });
+} else {
+  const stored = sessionStorage.getItem('quranPage');
+  if (stored) {
+    pagenum = parseInt(stored, 10);
+    sessionStorage.removeItem('quranPage');
+  }
+  
+  const iframe = document.getElementById('myiframe');
+  if (iframe) {
+    iframe.setAttribute('src', `https://www.easyquran-eg.com/qrhafs/page/${pagenum}`);
+  }
 
-document.getElementById(`player`).onclick = function(){
-    if(playerOpen == 0){
-        document.getElementById(`myiframe`).style.height = `calc(100% - 46px)`;
-        playerOpen = 1
-    }
-    else if(playerOpen == 1){
-        document.getElementById(`myiframe`).style.height = `100%`;
-        playerOpen = 0
-    }
-}
+  document.getElementById('next').onclick = function(){
+    pagenum++;
+    pagenum = pagenum >= 605 ? 604 : pagenum;
+    iframe.setAttribute('src', `https://www.easyquran-eg.com/qrhafs/page/${pagenum}`);
+  };
 
-document.getElementById(`dark`).onclick = function(){
-    if(darkmode == 0){
-        document.getElementById(`myiframe`).style.filter = `invert(100%) hue-rotate(180deg) contrast(110%)`;
-        darkmode = 1
+  document.getElementById('back').onclick = function(){
+    pagenum--;
+    pagenum = pagenum <= 0 ? 1 : pagenum;
+    iframe.setAttribute('src', `https://www.easyquran-eg.com/qrhafs/page/${pagenum}`);
+  };
+
+  document.getElementById('goto').onclick = function(){
+    const input = document.getElementById('pagenumber');
+    pagenum = parseInt(input.value, 10);
+    pagenum = pagenum <= 0 ? 1 : pagenum;
+    pagenum = pagenum >= 605 ? 604 : pagenum;
+    iframe.setAttribute('src', `https://www.easyquran-eg.com/qrhafs/page/${pagenum}`);
+    input.value = '';
+  };
+
+  document.getElementById('player').onclick = function(){
+    if (playerOpen === 0) {
+      iframe.style.height = 'calc(100% - 46px)';
+      playerOpen = 1;
+    } else {
+      iframe.style.height = '100%';
+      playerOpen = 0;
     }
-    else if(darkmode == 1){
-        document.getElementById(`myiframe`).style.filter = `none`;
-        darkmode = 0
+  };
+
+  document.getElementById('dark').onclick = function(){
+    if (darkmode === 0) {
+      iframe.style.filter = 'invert(100%) hue-rotate(180deg) contrast(110%)';
+      darkmode = 1;
+    } else {
+      iframe.style.filter = 'none';
+      darkmode = 0;
     }
+  };
 }
